c5d0bf4883f132ac4444415d950e652ebd01d621
Prithish44 <prithish.saha44@gmail.com>
2026-01-08 12:48:30 +0530

    Time: 39 ms (33.6%), Space: 15.4 MB (80%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -14,7 +14,7 @@ public:
 
         int val = nums1[i]*nums2[j];  
 
-        int take_i_j = (nums1[i]*nums2[j]) + solve(nums1, nums2, i + 1, j + 1); 
+        int take_i_j = val + solve(nums1, nums2, i + 1, j + 1); 
 
         int take_i = solve(nums1, nums2, i, j + 1);  
 
@@ -27,7 +27,7 @@ public:
         n = nums2.size();  
 
         memset(t, -1, sizeof(t));  
-        
+
         return solve(nums1, nums2, 0, 0);
     }
 };
