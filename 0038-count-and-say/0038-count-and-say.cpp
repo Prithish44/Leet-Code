fe625c4de1e056880c179ecf53f2beb0e9fcee10
Prithish44 <prithish.saha44@gmail.com>
2026-01-08 13:21:00 +0530

    Time: 59 ms (5.2%), Space: 125.2 MB (5.03%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -4,23 +4,30 @@ public:
         if(n == 1)
             return "1";  
         
+
         string say = countAndSay(n - 1);  
 
-        string result = "";  
+
+        string result = ""; 
+
+
         for(int i = 0; i < say.length(); i++)
         {
             char ch = say[i];  
             int count = 1;
 
-            while(i < say.length() && say[i] == say[i + 1])
+
+            while(i < say.length() - 1 && say[i] == say[i + 1])  //  say.length() - 1 As Compare say[i] == say[i + 1] No Out Of Bound  
             {
                 count = count + 1; 
                 i = i + 1;  
             }
 
-            result = result + to_string(count) + ch;
+
+            result = result + to_string(count) + ch;  //  string(ch, 1);  
         }
 
-        return result;  
+
+        return result;      
     }
 };
