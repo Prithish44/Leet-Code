bc98a293b8e8d580895a994df7e2c0e6cecd2b3d
Prithish44 <prithish.saha44@gmail.com>
2026-01-12 15:53:10 +0530

    Time: 2 ms (4.16%), Space: 10 MB (5.83%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -6,6 +6,10 @@ public:
         for(int &num : nums)
             mp[num] = mp[num] + 1;  
         
+
+        //  Remove Element Occur Only 1    
+
+
         for(auto it = mp.begin(); it != mp.end();)
         {
             if(it->second == 1)
@@ -14,13 +18,16 @@ public:
                 ++it;  
         }
 
-        int result = 0;  
+
+        int result = 0;    
+
 
         for(auto it = mp.begin(); it != mp.end(); it++)
         {
             int n = it->second;  
             result = result + (n*(n - 1)) / 2;  
         }
+
 
         return result;  
     }
