64b97b476e893fa28196a6768f2133be316ecdbe
Prithish44 <prithish.saha44@gmail.com>
2026-03-07 00:32:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (1.44%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int n = s.length();  
-
-
-        int count = 0;  
-
-
-        int i = 0;  
-
-
-        while(i < n)  
-        {
-            if(s[i] == '1')  
-            {
-                count = count + 1;  
-
-                while(i < n && s[i] == '1')  
-                    i = i + 1;  
-            }
-            else  
-                i = i + 1;  
-            
-
-            if(count > 1)  
-                return false;   
-        }  
-
-
-        return true;     
+       return s.find("01") == string :: npos;    
     }
 };
