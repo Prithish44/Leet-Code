de1a2cba332eba013dfd82d144d2bbd624570fae
Prithish44 <prithish.saha44@gmail.com>
2026-03-08 00:41:30 +0530

    Time: 41 ms (39.2%), Space: 28.3 MB (34.8%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,41 +1,66 @@
 class Solution {
 public:
     int minFlips(string s) {
-        int n = s.size();
-        s += s;
+        string s1;  
 
-        string s1, s2;
 
-        for(int i = 0; i < 2*n; i++) {
-            s1 += (i % 2 ? '0' : '1');
-            s2 += (i % 2 ? '1' : '0');
-        }
+        string s2;  
 
-        int i = 0, j = 0;
-        int result1 = 0, result2 = 0;
-        int result = INT_MAX;
 
-        while(j < 2*n) {
+        int n = s.length();    
 
-            // expand window
-            if(s[j] != s1[j]) result1++;
-            if(s[j] != s2[j]) result2++;
 
-            // shrink if window exceeds n
-            if(j - i + 1 > n) {
-                if(s[i] != s1[i]) result1--;
-                if(s[i] != s2[i]) result2--;
-                i++;
-            }
+        s = (s + s);   
 
-            // window size exactly n
-            if(j - i + 1 == n)
-                result = min({result, result1, result2});
 
-            j++;
-        }
+        for(int i = 0; i < 2*n; i++)  
+        {
+            s1 += (i % 2 ? '0' : '1');  
 
-        return result;
+
+            s2 += (i % 2 ? '1' : '0');    
+        }    
+
+
+        int result = INT_MAX;  
+
+
+        int flip1 = 0;  
+
+
+        int flip2 = 0;  
+
+
+        int i = 0;  
+
+
+        int j = 0;  
+
+
+        while(j < 2*n)  
+        {
+            if(s[j] != s1[j])  
+                flip1 = flip1 + 1;  
+            if(s[j] != s2[j])  
+                flip2 = flip2 + 1;  
+            if(j - i + 1 > n)  
+            {
+                if(s[i] != s1[i])  
+                    flip1 = flip1 - 1;  
+                if(s[i] != s2[i])  
+                    flip2 = flip2 - 1;  
+                
+
+                i = i + 1;  
+            }  
+            if(j - i + 1 == n)  
+                result = min({result, flip1, flip2});  
+            
+
+            j = j + 1; 
+        }  
+        
+
+        return result;     
     }
 };
-
