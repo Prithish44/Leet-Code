553fb5e07ac62893f4cb2ba0d34026d8686ea19d
Prithish44 <prithish.saha44@gmail.com>
2026-06-16 23:32:15 +0530

    Time: 3 ms (76.89%), Space: 39.9 MB (24.24%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -3,18 +3,21 @@ public:
     string processStr(string s) 
     {
         string result = "";      
-        
+
 
         for(char &ch : s)  
         {
-            if(islower(ch))  
-                result = result + ch;     
-            else if(ch == '*' && result.length() > 0)  
-                result.pop_back();    
+            if(ch == '*')   
+            {
+                if(result.length() > 0)     
+                    result.pop_back();    
+            }      
             else if(ch == '#')  
                 result = result + result;    
+            else if(ch == '%') 
+                reverse(begin(result), end(result));   
             else  
-                reverse(begin(result), end(result));  
+                result = result + ch;    
         }   
 
 
