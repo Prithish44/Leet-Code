dc256d4047c694f7638828763c3a799d201e0744
Prithish44 <prithish.saha44@gmail.com>
2026-07-21 00:00:05 +0530

    Time: 0 ms (100%), Space: 29.5 MB (94.52%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        int n = nums.size();  
+
+        k = k % n;   
+
+        if(k == 0)     
+            return;   
+
+        auto reverse = [&](int i, int j)  
+        {
+            while(i < j)  
+            {
+                swap(nums[i], nums[j]);  
+                i = i + 1;  
+                j = j - 1;  
+            }
+        };    
+
+
+        reverse(0, n - 1);  
+        reverse(0, k - 1);  
+        reverse(k, n - 1);     
+    }
+};
