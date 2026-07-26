4754e508322ca5b0ef86619e38ddbb6e981d5598
Prithish44 <prithish.saha44@gmail.com>
2026-07-26 21:21:57 +0530

    Time: 87 ms (59.34%), Space: 350.1 MB (82.73%) - LeetHub

--- 4001-aggregate-two-time-series/4001-aggregate-two-time-series.cpp
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& s1, vector<vector<int>>& s2) {
-        vector<vector<int>> ans;
-        int n1 = s1.size(), n2 = s2.size();
+    vector<vector<int>> aggregateTimeSeries(vector<vector<int>>& series1, vector<vector<int>>& series2) {
+
+        int n = series1.size();
+        int m = series2.size();
+
+        vector<vector<int>> res;
+        
         int i = 0, j = 0;
+        
+        while(i < n && j < m) {
 
-        while (i < n1 || j < n2) {
-            int t;
+            int time1 = series1[i][0];
+            int time2 = series2[j][0];
 
-            if (i == n1) {
-                t = s2[j][0];
-            } else if (j == n2) {
-                t = s1[i][0];
-            } else {
-                t = min(s1[i][0], s2[j][0]);
-            }
-
-            int x1 = 0;
-            if (i < n1) {
-                x1 = s1[i][1];
-            }
-
-            int x2 = 0;
-            if (j < n2) {
-                x2 = s2[j][1];
-            }
-
-            ans.push_back({t, x1 + x2});
-
-            if (i < n1 && s1[i][0] == t) {
+            if(time1 == time2) {
+                res.push_back({time1, series1[i][1] + series2[j][1]});
                 i++;
-            }
-
-            if (j < n2 && s2[j][0] == t) {
                 j++;
+            }
+            else if(time1 > time2) {
+                res.push_back({time2, series1[i][1] + series2[j][1]});
+                j++;
+            }
+            else {
+                res.push_back({time1, series1[i][1] + series2[j][1]});
+                i++;
             }
         }
 
-        return ans;
+        while(i < n) {
+            res.push_back({series1[i][0], series1[i][1]});
+            i++;
+        }
+
+        while(j < m) {
+            res.push_back({series2[j][0], series2[j][1]});
+            j++;
+        }
+
+        return res;
     }
 };
