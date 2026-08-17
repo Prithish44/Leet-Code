7218ed48a46b500289c3fd66c8f7a8b090be238b
Prithish44 <prithish.saha44@gmail.com>
2026-08-18 00:11:48 +0530

    Time: 748 ms (23.95%), Space: 29 MB (9.19%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:    
+    vector<vector<int>> t;   
+    int solve(int l, int r, vector<int> &cumSum)    
+    {
+        if(l >= r)    
+            return 0;  
+        
+        if(t[l][r] != -1)    
+            return t[l][r];   
+        
+        int score = 0;   
+        for(int mid = l; mid < r; mid++)  
+        {
+            int leftSum = cumSum[mid] - (l - 1 >= 0 ? cumSum[l - 1] : 0);  
+            int rightSum = cumSum[r] - cumSum[mid];     
+
+            if(leftSum < rightSum)  
+                score = max(score, leftSum + solve(l, mid, cumSum));  
+            else if(leftSum > rightSum)  
+                score = max(score, rightSum + solve(mid + 1, r, cumSum));  
+            else  
+                score = max({score, leftSum + solve(l, mid, cumSum), rightSum + solve(mid + 1, r, cumSum)});     
+        }      
+
+
+        return t[l][r] = score;     
+    }
+    int stoneGameV(vector<int>& stoneValue) 
+    {
+        int n = stoneValue.size();    
+
+        t.resize(n + 1, vector<int>(n + 1, -1));  
+
+        vector<int> cumSum(n);  
+
+        cumSum[0] = stoneValue[0];    
+
+        for(int i = 1; i < n; i++)  
+            cumSum[i] = stoneValue[i] + cumSum[i - 1];   
+
+
+        return solve(0, n - 1, cumSum);    
+    }
+};
